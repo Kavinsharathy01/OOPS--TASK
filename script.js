d3072a37de9217f780866dcb7cc494fb630c8f01
Kavinsharathy01 <166894740+Kavinsharathy01@users.noreply.github.com>
2024-06-15 01:19:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+ // write a "person" class to hold the details.....
+
+
+ class person{
+   constructor ("Name","Age","DOB","QUALIFICATION","Fathers name","Mothers name","Skills","Experience","Archivements"){
+      
+    this.Name            = Name;
+    this.age             = age;
+    this.DOB             = DOB;           
+    this.Qualification   = Qualification;  
+    this.Fathers name    = Fathers name; 
+    this.Mothers name    = Mothers name; 
+    this.Skills          = Skills;
+    this.Experience      = Experience;
+    this.Archivements    = Archivements; 
+      
+ }
+   }
+
+
+  // var res = new person  ("johndoe","28","16.05.1993","MBA","David","Christy","photography","5 years","foot ball gold medalist");
+  // console.log(res);
+  
+ // write a "MOVIE"  details.....
+
+class movie{
+    constructor(titlr,studio,rating){
+        this.title = title ;
+        this.studio = studio ;
+        this .rating = rating ;
+     }
+ }
+
+
+  var main = new ("Casino royale","Eon productions","PG13")
+
+
+// Writa a class to calculate the uber price 
+
+class uber{
+    
+    constructor (km,price){
+        this.km = km ;
+        this.price = price ; 
+    }
+    method  
+    
+    mini (km,price)
+     {value ==   km * price;
+        return value}
+    
+    
+    }
+    console.log (15*10 + " HAPPY JOURNY");
